9aff65fdaf7ec3369a9a2b78b4dfb000a03ee08c
ivanvb <ivanvalette@hotmail.com>
2021-06-19 20:33:32 -0400

    fix: header size on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,12 +17,16 @@ const Header = () => {
     `);
     return (
         <header className="z-50 -mb-40">
-            <div className="container flex items-center justify-between py-6 mb-5 md:mb-10">
+            <div className="container flex items-center justify-between py-8 mb-5 md:mb-10">
                 <h1>
-                    <img src={Logo} alt="Song Name Generator by Generators™️" />
+                    <img
+                        src={Logo}
+                        alt="Song Name Generator by Generators™️"
+                        className="h-6 xs:h-8 md:h-10"
+                    />
                 </h1>
                 <nav className="text-sm font-bold tracking-wide xs:text-lg md:text-xl">
-                    <ul className="flex space-x-2 xs:space-x-3 md:space-x-8">
+                    <ul className="flex items-center space-x-2 xs:space-x-3 md:space-x-8">
                         {navbar.map((item, i) => {
                             return (
                                 <li className="" key={i}>
