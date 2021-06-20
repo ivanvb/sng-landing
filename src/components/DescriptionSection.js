a3cef1313757a0706e29fecdcbda42e9167223b3
ivanvb <ivanvalette@hotmail.com>
2021-06-19 22:59:47 -0400

    fix: update DescriptionSection icon

--- src/components/DescriptionSection.js
+++ src/components/DescriptionSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { gsonghql, useStaticQuery } from 'gatsby';
+import AudioWave from '../images/AudioWave.svg';
 
 const DescriptionSection = () => {
     const {
@@ -17,9 +18,7 @@ const DescriptionSection = () => {
     return (
         <section className="py-12 bg-secondary-accent">
             <div className="container text-center">
-                <div className="">
-                    <div className="w-16 h-16 mx-auto mb-3 rounded-full bg-primary-accent"></div>
-                </div>
+                <img src={AudioWave} className="h-20 mx-auto mb-3" />
                 <h2 className="mb-2 text-2xl font-bold md:text-4xl">{description_section.title}</h2>
                 <p className="w-full mx-auto text-lg text-secondary-gray md:w-9/12">
                     {description_section.text}
