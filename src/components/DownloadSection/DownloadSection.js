5ab601d8b9c482e7aa8dad14ce7acf80653bb512
ivanvb <ivanvalette@hotmail.com>
2021-06-26 13:58:50 -0400

    feat: add DownloadSection animations

--- src/components/DownloadSection/DownloadSection.js
+++ src/components/DownloadSection/DownloadSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
 
 const DownloadSection = () => {
     const {
@@ -30,18 +32,55 @@ const DownloadSection = () => {
             }
         }
     `);
+
+    const { inView, ref } = useInView({ threshold: 0.8, triggerOnce: true });
+    const containerVariants = {
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+            },
+        },
+        hidden: {
+            y: 30,
+            opacity: 0,
+        },
+    };
+
+    const itemOffset = 1.5;
+    const itemVariants = {
+        visible: {
+            y: [itemOffset, itemOffset * -1, itemOffset],
+            transition: {
+                duration: 4,
+                repeat: Infinity,
+                ease: 'linear',
+            },
+        },
+        hidden: {
+            y: itemOffset * -1,
+        },
+    };
+
     return (
         <section
             className="relative flex flex-col items-center py-20 overflow-hidden border-b-8 bg-opacity-30 bg-primary-accent border-primary-accent"
             id="download"
+            ref={ref}
         >
-            <div className="container">
+            <motion.div
+                className="container"
+                variants={containerVariants}
+                animate={Object.keys(containerVariants)[inView ? 0 : 1]}
+            >
                 <h2 className="mb-4 text-2xl font-bold text-center md:mb-8 md:text-3xl">
                     {download.cta}
                 </h2>
                 <div className="flex justify-center space-x-4 md:space-x-16">
                     {[download.apple, download.android].map((item, i) => (
-                        <a
+                        <motion.a
+                            variants={itemVariants}
                             href={item.link}
                             className="inline-block w-40 h-16 md:w-48 md:h-16"
                             key={i}
@@ -52,10 +91,10 @@ const DownloadSection = () => {
                                 className="w-full h-full"
                                 objectFit="contain"
                             />
-                        </a>
+                        </motion.a>
                     ))}
                 </div>
-            </div>
+            </motion.div>
             <div className="absolute bottom-0 w-full translate-y-1/2">
                 <div className="absolute bottom-0 w-full h-12 rounded-b-none bg-primary-accent rounded-1/2 blur-2xl animate-shine"></div>
             </div>
